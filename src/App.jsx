bfa942083c42015f6c6bc4f5be0585f816bf7b3a
Sunday-Binakin <sundaybinakin@gmail.com>
2024-10-01 03:37:49 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+ 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <header>
+        <nav className="navbar">
+          <div className="nav-icon"><a className='startup-text' href="#">Startup</a></div>
+          <div className="nav-list">
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Services</a></li>
+              <li><a href="">Blog</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a className='join-us-button' href="">Join Us</a></li>
+            </ul>
+          </div>
+        </nav>
+       </header>
+    </>
+  )
+}
+
+export default App
